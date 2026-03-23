0a8ee2542cc709da2e9f106f34e1a57d2e54a375
sansalabsblockchain-cyber <sansalabsblockchain@gmail.com>
2026-03-23 16:07:06 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://viva-amazonia-b7d621e98f23.herokuapp.com/",
-  // baseURL: "http://localhost:3001/",
+  baseURL: 'https://hope-green-api.vercel.app/'
 });
 
 export { api };
